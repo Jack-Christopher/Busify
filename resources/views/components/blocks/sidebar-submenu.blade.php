41f9bf49121b76e041a81ce72ff1cf36c4fe42c5
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-05-05 16:42:30 -0500

    feat: add Alpine keywords

--- resources/views/components/blocks/sidebar-submenu.blade.php
+++ resources/views/components/blocks/sidebar-submenu.blade.php
@@ -1,10 +1,12 @@
 <div class="flex flex-col space-y-2"
-    :class="{'block': selected == '{{ $name }}', 'hidden': selected != '{{ $name }}' }">
+    x-cloak x-show="selected == '{{ $name }}'">
     <hr class="my-2 border-gray-200 dark:border-gray-700">
 
     @for ($i = 0; $i < count($options); $i++)
         @foreach ($options[$i] as $op)
-            <button class="float-left">
+            <button @click="option = '{{ $op }}', isVisible = false"
+            {{-- on hover do something --}}
+                class="float-left hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-700 dark:hover:text-gray-200 hover:shadow-lg hover:rounded-lg hover:font-bold hover:cursor-pointer hover:transition hover:duration-150 hover:ease-in-out">
                 {{ $op }}
             </button>
         @endforeach
