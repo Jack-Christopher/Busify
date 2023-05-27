25cda2528c13bbdc7f949d7994bde0b9e40c1b38
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-05-26 20:13:28 -0500

    feat: add function to get name of Ubigeo

--- app/Models/UbigeoZone.php
+++ app/Models/UbigeoZone.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ubigeo;
 
 class UbigeoZone extends Model
 {
     use HasFactory;
 
     protected $table = 'ubigeo_zone';
+
+
+    public function ubigeoName()
+    {
+        $ubigeo = Ubigeo::where('code', $this->ubigeo_id)->first();
+        return $ubigeo->region . ' - ' . $ubigeo->province . ' - ' . $ubigeo->district;
+    }
 }
