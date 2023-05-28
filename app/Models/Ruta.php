fbbb21b94823438caceaa414bd7e28ade31a343c
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-05-28 12:06:20 -0500

    feat: add relationships and functions for the Ruta model

--- app/Models/Ruta.php
+++ app/Models/Ruta.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\UbigeoZone;
 
 class Ruta extends Model
 {
     use HasFactory;
+
+    public function viajes(): HasMany
+    {
+        return $this->hasMany(Viaje::class);
+    }
+
+    public function zona(): BelongsTo
+    {
+        return $this->belongsTo(Zona::class);
+    }
+
+
+
+    public function getUbigeos(): array
+    {
+        $ubigeos = [];
+        
+        $ubigeoZones = UbigeoZone::where('zone_id', $this->zona_id)
+            ->where('position', '>=', $this->origen_id)
+            ->where('position', '<=', $this->destino_id)
+            ->orderBy('position', 'asc')
+            ->get();
+
+        foreach ($ubigeoZones as $ubigeoZone) {
+            $ubigeos[] = $ubigeoZone->ubigeo;
+        }
+
+        return $ubigeos;
+    }
 }
